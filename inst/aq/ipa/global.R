822170027327d4b6b63990d817d2ddb591364155
adityaparama <53287306+adityaparama@users.noreply.github.com>
2019-07-27 16:04:59 +0700

    Add files via upload

---
+++ inst/aq/ipa/global.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(here)
+library(recipes)
+library(ranger)
+library(parsnip)
+
+load(here("data", "un_smp_recipe.rda"))
+load(here("data", "un_smp_lm.rda"))
+load(here("data", "un_smp_rf.rda"))
